5297365a416adc5fb5949ab7651b83ac2721018e
Crophics <julian.r.piegari@icloud.com>
2026-08-28 16:32:41 -0400

    restore hidden portal text CSS

--- portal/conf.py
+++ portal/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'My Project'
+project = 'portal'
 copyright = '2026, Julian'
 author = 'Julian'
-release = '0.1'
+release = '0.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -28,4 +28,5 @@ html_theme = 'furo'
 html_static_path = ['_static']
 html_show_sourcelink = False
 html_logo = '_static/logo.avif'
-html_title = ' '
+html_title = 'Portal'
+html_css_files = ['custom.css']
